a12bcf5e849d7af171136a395fd02b91e3494df7
Tom CHEN <tomchen.org@gmail.com>
2020-04-12 23:57:36 +0200

    example and readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FNT Generator
 Might and Magic 6 7 8 and Heroes 3 FNT (font) File Generator for Multiple Languages (Python Script)
+
+Usage: see [main.py](https://github.com/might-and-magic/fnt-generator/blob/master/main.py) as an example
--- main.py
+++ main.py
@@ -1,27 +1,46 @@
 import fntgen as f
 
+
+# Put the .bdf font file(s) in a folder, `cd` to this folder,
+# then uncomment, modify and execute the following code
+
+
+# unifont for various languages:
+
 # Bdf = f.FntGen('unifont-9.0.06.bdf')
-# Bdf.setCharset('gb2312').setDecoration('shadow').output('DBCS_<SIZE>_', 'uni/')
+# Bdf.setCharset('cp1252').setDecoration('shadow').output('FONT_', 'uni_cp1252')
+
+# Bdf = f.FntGen('unifont-9.0.06.bdf')
+# Bdf.setCharset('cp1251').output('FONT_', 'uni_cp1251')
+
+# Bdf = f.FntGen('unifont-9.0.06.bdf')
+# Bdf.setCharset('cp1250').setDecoration('black').output('FONT_', 'uni_cp1250')
+
+# Bdf = f.FntGen('unifont-9.0.06.bdf')
+# Bdf.setCharset('gb2312').setDecoration('glow').output('DBCS_<SIZE>_', 'uni_gb2312')
+
+# Bdf = f.FntGen('unifont-9.0.06.bdf')
+# Bdf.setCharset('big5').output('DBCS_<SIZE>_', 'uni_big5')
+
+
+
+# For MM6-8 zh_CN:
 
 # Bdf = f.FntGen('SimSun-12.bdf')
-# Bdf.setCharset('gb2312').setDecoration('shadow').output('DBCS_<SIZE>_', 's12/')
+# Bdf.setCharset('gb2312').setDecoration('shadow').output('DBCS_<SIZE>_', 'zh_CN_13/')
 
 # Bdf = f.FntGen('SimSun-14.bdf')
-# Bdf.setCharset('gb2312').setDecoration('shadow').output('DBCS_<SIZE>_', 's14/')
+# Bdf.setCharset('gb2312').setDecoration('black').output('DBCS_<SIZE>b_', 'zh_CN_15b/')
 
 # Bdf = f.FntGen('STKaiti-19.bdf')
-# Bdf.setCharset('gb2312').setDecoration('black').output('DBCS_<SIZE>_', 'kai19/')
+# Bdf.setCharset('gb2312').setDecoration('black').output('DBCS_<SIZE>_', 'zh_CN_16/')
 
 # Bdf = f.FntGen('FZCKJW-GB1-0-26.bdf')
-# Bdf.setCharset('gb2312').setDecoration('glow').output('DBCS_<SIZE>_', 'fz26/')
+# Bdf.setCharset('gb2312').setDecoration('glow').output('DBCS_<SIZE>_', 'zh_CN_29/')
+
+
+
+# For MM6-8 zh_TW:
 
 # Bdf = f.FntGen('HanWangYanKai-26.bdf')
-# Bdf.setCharset('gb2312').setDecoration('glow').output('DBCS_<SIZE>_', 'yankai26/')
-
-Bdf = f.FntGen('SimSun-14.bdf')
-Bdf.setCharset('gb2312').setDecoration('black').output('DBCS_<SIZE>b_', 's14/')
-
-# 27 glow kai
-# 15 shadow song
-# 15 normal song
-# 12 shadow song
+# Bdf.setCharset('big5').setDecoration('glow').output('DBCS_<SIZE>_', 'zh_TW_29/')
